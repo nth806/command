55fa11302ae9e01fc1b1474289d1c458a54f9642
NguyenTrongHieu <hieu.nt@tctav.com>
2016-11-25 14:33:04 +0700

    Push2Client: add checking emails before pulling source from remote client repository

--- _inc/git.sh
+++ _inc/git.sh
@@ -8,3 +8,23 @@ function git_currentBranch () {
 
   echo ${branch_name}
 }
+
+function git_statusBranch () {
+  local l_UPSTREAM='@{u}'
+  local l_LOCAL=$(git rev-parse @)
+  local l_REMOTE=$(git rev-parse "$l_UPSTREAM")
+  local l_BASE=$(git merge-base @ "$l_UPSTREAM")
+
+  if [ "x$l_LOCAL" = "x$l_REMOTE" ]; then
+    # "Up-to-date"
+    return 0
+  elif [ "x$l_LOCAL" = "x$l_BASE" ]; then
+  	# Need to pull
+    return 1
+  elif [ "x$l_REMOTE" = "x$l_BASE" ]; then
+  	# Need to push
+    return 2
+  fi
+
+  return -1
+}
--- fn/push2client/sync.sh
+++ fn/push2client/sync.sh
@@ -1,3 +1,82 @@
+function __add_email_list () {
+  local line=`echo ${1} | sed -e 's/\r//g'`
+  line=`cmn_trimSpaces "${line}"`
+
+  cmn_isStartWith '#' ${line}
+  if [ $? -eq 0 ] ; then
+    return
+  fi
+
+  if [ "x${line}" = "x" ]; then
+    return
+  fi
+
+  EMAIL_LIST+=("${line}")
+}
+
+
+function __verify_client_repo () {
+  if [ 'x'`git_currentBranch` != 'x'"${GIT_DEFAULT_BRANCH}" ]
+  then
+    git checkout master
+    if [ $? -ne 0 ]
+    then
+      cmn_exitAbnormal 'Checking out branch <${GIT_DEFAULT_BRANCH}> (client repository) was failed, please check again'
+    fi
+  fi
+
+  git fetch origin "${GIT_DEFAULT_BRANCH}"
+  if [ $? -ne 0 ]
+  then
+    cmn_exitAbnormal 'Fetching <${GIT_DEFAULT_BRANCH}> (client repository) was failed, please check again'
+  fi
+
+  git_statusBranch
+  local stt=$?
+  if [ $stt -eq 0 ] ; then
+    # Up to date
+    return 0
+  fi
+
+  if [ $stt -eq 2 ] ; then
+    # Go ahead remote branch
+    cmn_exitAbnormal 'You need to push souce to client repository before continuing'
+  fi
+
+  if [ $stt -ne 1 ] ; then
+    cmn_exitAbnormal 'Your local repository is deverged'
+  fi
+
+  # Email
+  local line=
+  local email=
+  EMAIL_LIST=()
+  if [ ! -f "${EMAIL_FILE}" ] ; then
+    cmn_exitAbnormal "Does not exist email file <${EMAIL_FILE:3}>"
+  fi
+
+  PRI_IFS=$IFS
+  export IFS=$'\n'
+  while read line; do
+    __add_email_list "${line}"
+  done < ${EMAIL_FILE}
+  __add_email_list "${line}"
+  export IFS=$PRI_IFS
+
+  if [ ${#EMAIL_LIST[@]} -eq 0 ] ; then
+    cmn_exitAbnormal 'Have not set email list yet'
+  fi
+
+  for email in `git log --pretty=format:'%ae' HEAD..origin/${GIT_DEFAULT_BRANCH}`
+  do
+    if [[ ! " ${EMAIL_LIST[@]} " =~ " ${email} " ]]; then
+      cmn_exitAbnormal "There is a commit by <${email}> on remote repository. Please ask your leader how to solve this."
+    fi
+  done
+
+  git pull origin "$GIT_DEFAULT_BRANCH"
+}
+
 function push2client_sync () {
   cmn_showTitleStep "Synchronize source"
 
@@ -17,14 +96,6 @@ function push2client_sync () {
   fi
 
   cd ${CLIENT_DIR}
-  if [ 'x'`git_currentBranch` != 'x'"${GIT_DEFAULT_BRANCH}" ]
-  then
-    git checkout master
-    if [ $? -ne 0 ]
-    then
-      cmn_exitAbnormal 'Checking out branch <${GIT_DEFAULT_BRANCH}> (client repository) was failed, please check again'
-    fi
-  fi
-
+  __verify_client_repo
   cd ${BASE_DIR}
 }
