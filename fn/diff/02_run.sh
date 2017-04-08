cfda9148da8122ce38e1784124ed7a2a43726761
Nguyen Trong Hieu <nth806@gmail.com>
2017-04-08 20:47:34 +0700

    Diff with parameters

--- fn/diff/02_run.sh
+++ fn/diff/02_run.sh
@@ -1,13 +1,24 @@
 function diff_run () {
   comp_show_title_step 'Run diff'
 
-  DIFF_FUNCTION=diff
-  DIFF_MERGE=sgdm
+  local is_tool=1
+  for i; do
+    if [ "x${i}" = "x-t" ]; then
+      is_tool=0
+      break
+    fi
+  done
 
-  which ${DIFF_MERGE} 2> /dev/null 1> /dev/null
-  if [ $? -eq 0 ] ; then
-    DIFF_FUNCTION=$DIFF_MERGE
+  if [ $is_tool -eq 0 ]; then
+    which sgdm 2> /dev/null 1> /dev/null
+    if [ $? -eq 0 ]; then
+      sgdm "${TTV_SRC}/${FILE_DIFF_PATH}" "${CLIENT_DIR}/${FILE_DIFF_PATH}"
+      return
+    elif [ -x /Applications/DiffMerge.app/Contents/MacOS/DiffMerge ]; then
+      /Applications/DiffMerge.app/Contents/MacOS/DiffMerge "${TTV_SRC}/${FILE_DIFF_PATH}" "${CLIENT_DIR}/${FILE_DIFF_PATH}"
+      return
+    fi
   fi
 
-  $DIFF_FUNCTION "${TTV_SRC}/${FILE_DIFF_PATH}" "${CLIENT_DIR}/${FILE_DIFF_PATH}"
+  diff $@ "${TTV_SRC}/${FILE_DIFF_PATH}" "${CLIENT_DIR}/${FILE_DIFF_PATH}"
 }
